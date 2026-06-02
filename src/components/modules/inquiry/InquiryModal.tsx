76912ca8f574865dc1db67e2150470d47dd2a894
GJ5PLUS LEDTV <gj5plus.ledtv@gmail.com>
2026-06-02 22:10:07 +0000

    URGENT FIX
    Runtime Error:
    ReferenceError: Separator is not defined
    
    Fil

--- src/components/modules/inquiry/InquiryModal.tsx
+++ src/components/modules/inquiry/InquiryModal.tsx
@@ -19,6 +19,7 @@ import {
   SelectValue 
 } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
+import { Separator } from '@/components/ui/separator';
 import { 
   UserPlus, 
   ChevronRight, 
